dc44f4216c7a3e75b9eda744f64523ed85f9a2d7
achuchulev <42144722+achuchulev@users.noreply.github.com>
2018-08-13 17:39:05 +0300

    Update main.tf

--- main.tf
+++ main.tf
@@ -3,8 +3,8 @@ resource "null_resource" "helloWorld1" {
     command = "echo hello word1"
   }
 }
-resource "null_resource" "helloWorld2" {
+resource "null_resource" "helloWorld0" {
   provisioner "local-exec" {
-    command = "echo hello word2"
+    command = "echo hello word0"
   }
 }
