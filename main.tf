89f95c3edd80ab120d87e824be99ca6aa3bee169
achuchulev <42144722+achuchulev@users.noreply.github.com>
2018-12-06 09:22:21 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,15 +1,16 @@
-resource "null_resource" "helloWorld" {
+resource "null_resource" "MultiHelloWorld" {
   provisioner "local-exec" {
-    command = "echo Hello world!"
+    count = 2
+    command = "echo Hello world ${count.index} !"
   }
 }
-resource "null_resource" "helloWorld1" {
-  provisioner "local-exec" {
-    command = "echo Hello parallel world 1!"
-  }
-}
-resource "null_resource" "helloWorld2" {
-  provisioner "local-exec" {
-    command = "echo Hello parallel world 2!"
-  }
-}
+#resource "null_resource" "helloWorld1" {
+#  provisioner "local-exec" {
+#    command = "echo Hello parallel world 1!"
+#  }
+#}
+#resource "null_resource" "helloWorld2" {
+#  provisioner "local-exec" {
+#    command = "echo Hello parallel world 2!"
+#  }
+#}
