ab38e19c28e9ea068c5afa0cbfd5a201ecb46c23
achuchulev <42144722+achuchulev@users.noreply.github.com>
2018-12-06 10:31:30 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,6 +1,6 @@
 resource "null_resource" "MultiHelloWorld" {
   count = "${var.multiple}"
   provisioner "local-exec" {
-    command = "echo Hello world (${count.index} + 1)!"
+    command = "echo Hello world ${count.index + 1}!"
   }
 }
