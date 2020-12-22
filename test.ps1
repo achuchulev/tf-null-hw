f2d421df54c4d4a216ca809c04350297880297e6
Atanas Chuchulev <atanas.v4@gmail.com>
2020-12-22 17:43:21 +0200

    Delete test.ps1

--- test.ps1
+++
@@ -1,3 +0,0 @@
-$apimContext = New-AzApiManagementContext -ResourceGroupName "atanas-test-rg" -ServiceName "atanas-test-api-management"
-
-Get-AzApiManagementSubscription -Context $apimContext -Scope "/apis"
